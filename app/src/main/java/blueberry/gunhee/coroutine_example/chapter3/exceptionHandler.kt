4d725cd95cd38e6709e82aaa4e040724e3df40dc
이건희 <leech9876@naver.com>
2026-04-11 22:52:24 +0900

    fix: exceptionHandler가 정상적으로 안되던것 수정

--- app/src/main/java/blueberry/gunhee/coroutine_example/chapter3/exceptionHandler.kt
+++ app/src/main/java/blueberry/gunhee/coroutine_example/chapter3/exceptionHandler.kt
@@ -1,6 +1,7 @@
 package blueberry.gunhee.coroutine_example.chapter3
 
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -10,7 +11,8 @@ fun main() = runBlocking {
         coroutineContext, throwable ->
         println("Job cancelled due to ${throwable.message} [$coroutineContext]")
     }
-    launch(exceptionHandler) {
+    // runBlocking 환경에서는 해당 테스트가 불가능함 (새로운 스코프를 만들어줘야하므로 Job() 추가)
+    launch(exceptionHandler + Job()) {
         TODO("Not implemented yet!")
     }
 
